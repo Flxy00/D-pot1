7b33ee80548a4b2552054e65f6f32fb28aaaccc0
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-11 12:04:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 st.title('This is a title')
 st.title('_Streamlit_ is :blue[cool] :burger:')
 st.write("Hello")
-import streamlit as st
 name=st.text_input("Your name")
 st.write("hello" + name)
+col1, col2, col3 = st.columns(3)
+col1.metric("Temperature", "70 °F", "1.2 °F")
+col2.metric("Wind", "9 mph", "-8%")
+col3.metric("Humidity", "86%", "4%")
