1f669d553971bcafc0f3aa7ae5ee9146688f05c3
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-11 12:02:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title('This is a title')
-st.title('_Streamlit_ is :blue[cool] :sunglasses:')
+st.title('_Streamlit_ is :blue[cool] :burger:')
 st.write("Hello")
 import streamlit as st
 name=st.text_input("Your name")
