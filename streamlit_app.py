8d38941e9144fbb9e7a109235bbac262701a5cb5
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 12:45:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ word_fr= voc['Définition'].values[i]
 word_chi= voc['Hanzi'].values[i]
 st.write(word_fr+""+ word_chi)
 st.button("refresh")
+indices=np.random.choice(l,site=4,replace=Fake)
+st.write(indices)
+indices[0],indices[3]
+for i in range(4):
+  st.button(voc["Hanzi"].values[indices[i]])
