098240fa054ae4df136c3802c0b515898f18d2de
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 11:36:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 import pandas as pd
+
 voc=pd.read_CSV('https://docs.google.com/spreadsheets/d/1ZVqjn50wq2PFTtrWIr_Dc8mfuJfHJUFcmRNjtA2LSv0/edit?usp=sharing')
 st.dataframe(voc)
