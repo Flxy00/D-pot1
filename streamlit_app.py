ae986b57f202614545da0f15b9b5800224781d98
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 11:46:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-voc=pd.read_CSV ('https://docs.google.com/spreadsheets/d/e/2PACX-1vST1KuuolQSsMjI6Xemgo6a1CD5ys4RGhOYDntzJh-JylhPF2C7_D0ecpQFRTkhhhV-SVyC22-beYBd/pub?output=csv')
+voc=pd.read_CSV("https://docs.google.com/spreadsheets/d/e/2PACX-1vST1KuuolQSsMjI6Xemgo6a1CD5ys4RGhOYDntzJh-JylhPF2C7_D0ecpQFRTkhhhV-SVyC22-beYBd/pub?output=csv")
 st.dataframe(voc)
