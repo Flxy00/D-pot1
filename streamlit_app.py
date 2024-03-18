6f5d14a3a19ebe59e878e44350316510e767e146
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 12:05:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-voc=pd.read_CSV('https://docs.google.com/spreadsheets/d/e/2PACX-1vST1KuuolQSsMjI6Xemgo6a1CD5ys4RGhOYDntzJh-JylhPF2C7_D0ecpQFRTkhhhV-SVyC22-beYBd/pub?gid=0&single=true&output=csv')
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vST1KuuolQSsMjI6Xemgo6a1CD5ys4RGhOYDntzJh-JylhPF2C7_D0ecpQFRTkhhhV-SVyC22-beYBd/pub?gid=0&single=true&output=csv')
 l=voc.shape[0]
 i=np.random.choice(range(l))
 word_fr= voc['Définition'].values[i]
