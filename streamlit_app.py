6de36fc6273807adad6f991a4dbca04da854dcc4
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 11:32:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
+import pandas as pd
 voc=pd.read_CSV('https://docs.google.com/spreadsheets/d/1ZVqjn50wq2PFTtrWIr_Dc8mfuJfHJUFcmRNjtA2LSv0/edit?usp=sharing')
 st.dataframe(voc)
