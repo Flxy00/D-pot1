8321d38dd7308d29362e00a05a3d8b7b2627c347
Flxy00 <162983177+Flxy00@users.noreply.github.com>
2024-03-18 12:53:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,9 @@ j=st.write(indices)
 word_fr=voc["Définition"].values[j]
 st.write("Traduis:"+word_fr)
 for i in range(4):
-  st.button(voc["Hanzi"].values[indices[i]])
+  st.button(voc["Hanzi"].values[indices[i]], on_clich=is_correct,args=[indices[i],j])
+def is_correct(i,j):
+    if i==j
+  st.write("Bravo")
+else:
+st.write("Raté")
